6a7de3d721f98f0759c12734bb21a5375839bbdd
jyliana <jyliana@gmail.com>
2023-01-12 16:52:34 +0200

    Adding servlet filter

--- src/main/java/com/example/system/controller/OrderController.java
+++ src/main/java/com/example/system/controller/OrderController.java
@@ -5,17 +5,17 @@ import com.example.system.model.User;
 import com.example.system.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
 
-@Controller
+@RestController
 public class OrderController {
 
     @Autowired
